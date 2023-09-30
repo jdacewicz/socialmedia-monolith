56d3927f0da697c11c6cabb9dd7fa409b2bf70ce
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-09-30 15:48:54 +0200

    Implemented Post ControllerAdvice

---
+++ src/main/java/pl/jdacewicz/socialmediaserver/post/PostControllerAdvice.java
@@ -0,0 +1,19 @@
+package pl.jdacewicz.socialmediaserver.post;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.jdacewicz.socialmediaserver.ApiError;
+
+@RestControllerAdvice
+class PostControllerAdvice {
+
+    @ExceptionHandler(PostNotFoundException.class)
+    ResponseEntity<ApiError> handlePostNotFoundException(PostNotFoundException exception) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        ApiError error = new ApiError(status.value(), exception.getMessage());
+        return ResponseEntity.status(status)
+                .body(error);
+    }
+}
--- src/main/java/pl/jdacewicz/socialmediaserver/post/PostNotFoundException.java
+++ src/main/java/pl/jdacewicz/socialmediaserver/post/PostNotFoundException.java
@@ -1,5 +1,9 @@
 package pl.jdacewicz.socialmediaserver.post;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 class PostNotFoundException extends RuntimeException {
 
     PostNotFoundException(String message) {

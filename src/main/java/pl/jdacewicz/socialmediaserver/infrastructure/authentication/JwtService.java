8c4138f5c6629583c0fe1034c3c0829e1acef46c
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-10-01 20:39:53 +0200

    Implemented generating jwt token

--- src/main/java/pl/jdacewicz/socialmediaserver/infrastructure/authentication/JwtService.java
+++ src/main/java/pl/jdacewicz/socialmediaserver/infrastructure/authentication/JwtService.java
@@ -36,6 +36,28 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return buildToken(extraClaims, userDetails, jwtExpiration);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateFreshToken(UserDetails userDetails) {
+        return buildToken(new HashMap<>(), userDetails, refreshExpiration);
+    }
+
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSignInKey())

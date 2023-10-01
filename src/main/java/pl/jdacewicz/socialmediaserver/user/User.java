c807d936d132642eb5ffd15ec0d9ea024222cac5
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-10-01 16:29:21 +0200

    Created User entity

---
+++ src/main/java/pl/jdacewicz/socialmediaserver/user/User.java
@@ -0,0 +1,65 @@
+package pl.jdacewicz.socialmediaserver.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "t_users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+class User implements UserDetails {
+
+    final static String USERS_STORE_DIRECTORY_URL = "data/users";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String profilePictureName;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

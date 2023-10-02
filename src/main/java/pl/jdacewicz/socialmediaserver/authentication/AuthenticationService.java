9e795b63259b6e7924d847a194d9244713c67d54
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-10-02 16:21:15 +0200

    Implemented registering and authenticating

---
+++ src/main/java/pl/jdacewicz/socialmediaserver/authentication/AuthenticationService.java
@@ -0,0 +1,42 @@
+package pl.jdacewicz.socialmediaserver.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.jdacewicz.socialmediaserver.authentication.dto.AuthenticationRequest;
+import pl.jdacewicz.socialmediaserver.authentication.dto.AuthenticationResponse;
+import pl.jdacewicz.socialmediaserver.authentication.dto.RegisterRequest;
+import pl.jdacewicz.socialmediaserver.user.User;
+import pl.jdacewicz.socialmediaserver.user.UserDetailsFacade;
+
+@Service
+@RequiredArgsConstructor
+class AuthenticationService {
+
+    private final UserDetailsFacade userDetailsFacade;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .build();
+        var createdUser = userDetailsFacade.createUser(user);
+        var jwtToken = jwtService.generateToken(createdUser);
+        return new AuthenticationResponse(jwtToken);
+    }
+
+    AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()));
+        var user = userDetailsFacade.getUserByEmail(request.email());
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
+    }
+}

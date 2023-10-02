091e7b625e886717908a017086b3a371cb576d24
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-10-02 16:19:57 +0200

    Implemented AuthenticationController

---
+++ src/main/java/pl/jdacewicz/socialmediaserver/authentication/infrastructure/web/AuthenticationController.java
@@ -0,0 +1,29 @@
+package pl.jdacewicz.socialmediaserver.authentication.infrastructure.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.jdacewicz.socialmediaserver.authentication.AuthenticationFacade;
+import pl.jdacewicz.socialmediaserver.authentication.dto.AuthenticationRequest;
+import pl.jdacewicz.socialmediaserver.authentication.dto.AuthenticationResponse;
+import pl.jdacewicz.socialmediaserver.authentication.dto.RegisterRequest;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationFacade authenticationFacade;
+
+    @PostMapping("/register")
+    public AuthenticationResponse register(@RequestBody RegisterRequest request) {
+        return authenticationFacade.register(request);
+    }
+
+    @PostMapping("/authenticate")
+    public AuthenticationResponse authenticate(@RequestBody AuthenticationRequest request) {
+        return authenticationFacade.authenticate(request);
+    }
+}

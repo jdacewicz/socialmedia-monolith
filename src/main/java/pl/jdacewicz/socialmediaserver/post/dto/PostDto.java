4d012a527f90f3a9f15b5349c62c2a0b115d2812
Jakub Dacewicz <99081886+jdacewicz@users.noreply.github.com>
2023-10-05 18:30:57 +0200

    Extended PostDto

--- src/main/java/pl/jdacewicz/socialmediaserver/post/dto/PostDto.java
+++ src/main/java/pl/jdacewicz/socialmediaserver/post/dto/PostDto.java
@@ -1,12 +1,15 @@
 package pl.jdacewicz.socialmediaserver.post.dto;
 
+import pl.jdacewicz.socialmediaserver.comment.dto.CommentDto;
 import pl.jdacewicz.socialmediaserver.user.dto.UserDto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record PostDto(long id,
                       LocalDateTime creationDateTime,
                       String content,
                       String imageUrl,
-                      UserDto creator) {
+                      UserDto creator,
+                      List<CommentDto> comments) {
 }
